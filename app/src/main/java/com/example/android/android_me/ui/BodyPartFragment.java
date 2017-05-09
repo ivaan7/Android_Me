84b45838a7cc5ff390158917d7116b5fe6c52abd
ivaan7 <ivaanboss7@gmail.com>
2017-05-09 16:45:12 +0200

    added 3 fragments and contaners for displaying images with fragment transactions

---
+++ app/src/main/java/com/example/android/android_me/ui/BodyPartFragment.java
@@ -0,0 +1,45 @@
+package com.example.android.android_me.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.android.android_me.R;
+
+import java.util.List;
+
+public class BodyPartFragment extends Fragment {
+
+    //Mandatory empty constructor for instantiating fragment
+    public BodyPartFragment() {
+    }
+
+    private List<Integer> mImageIds;
+    private int mListIndex;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_body_part, container, false);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.body_part_image_view);
+        if (mImageIds!=null){
+            imageView.setImageResource(mImageIds.get(mListIndex));
+        }else {
+            Log.i("IMAGE ID" , "no selected image");
+        }
+        return rootView;
+    }
+
+    public void setmImageIds(List<Integer> mImageIds) {
+        this.mImageIds = mImageIds;
+    }
+
+    public void setmListIndex(int mListIndex) {
+        this.mListIndex = mListIndex;
+    }
+}
